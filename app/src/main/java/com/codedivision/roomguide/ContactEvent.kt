cdbf6730fab25594616bbe5b175187be4e4ac21d
Samuel Bourassa <district67@gmail.com>
2023-03-27 01:52:32 -0400

    Add ContactEvent sealed interface that contains all the different user triggered events.

---
+++ app/src/main/java/com/codedivision/roomguide/ContactEvent.kt
@@ -0,0 +1,18 @@
+package com.codedivision.roomguide
+
+/**
+ *
+ * This interface contains all the different user triggered events to be handled
+ * by the ViewModel later on.
+ *
+ */
+sealed interface ContactEvent {
+    object SaveContact: ContactEvent
+    data class SetFirstName(val firstName: String): ContactEvent
+    data class SetLastName(val lastName: String): ContactEvent
+    data class SetPhoneNumber(val phoneNumber: String): ContactEvent
+    object ShowDialog: ContactEvent
+    object HideDialog: ContactEvent
+    data class SortContacts(val sortType: SortType): ContactEvent
+    data class DeleteContact(val contact: Contact): ContactEvent
+}
---
+++ app/src/main/java/com/codedivision/roomguide/SortType.kt
@@ -0,0 +1,7 @@
+package com.codedivision.roomguide
+
+enum class SortType {
+    FIRST_NAME,
+    LAST_NAME,
+    PHONE_NUMBER
+}
